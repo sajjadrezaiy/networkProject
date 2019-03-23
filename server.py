64f7e0fd9b0a7cc72f24c40a1ca049c92b8c6d90
sajjad <sajjadrezai5@gmail.com>
2019-03-23 20:28:00 +0430

    newest server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import torndb
+# import torndb
 import tornado.httpserver
 import tornado.ioloop
 import os
@@ -13,31 +13,27 @@ define("mysqldatabase",default="sr_db")
 define("mysqluser",default="root")
 define("mysqlpassword",default="13787638")
 import mysql.connector
+import re
 
 
-# class Database:
-#
-#     dbseting={
-#         "host":'127.0.0.1',
-#         'database':'networkproject',
-#         'user':'root',
-#         'password':13787638,
-#         'charset':'utf8'
-#
-#     }
-#     db=mysql.connector.connect(**dbseting)
-#
-#
-#
-#
-#     # def insert():
+class Database:
+    DATABASE_ = mysql.connector.connect(host='localhost', database='network', user='root', password='s@mba123@li14')
+
+
+
+
+
+
 
 
 class Aplication(tornado.web.Application):
     def __init__(self):
        handlers =[
            (r'/signup',connect),
-           (r'/login\w+',login)
+           (r'/login.*',login),
+           (r"/logout.*",logout),
+           (r'/sendticket.*',sendticket)
+
        ]
 
        setting = dict()
@@ -53,9 +49,21 @@ class BaseHandler(tornado.web.RequestHandler):
     def check_userpass(self,username,password):
         result=BaseHandler.database.get("SELECT * FROM user WHERE username=%s and password=%s ",username,password)
 
+    def check_user(self,username):
+        result=BaseHandler.database.get("SELECT * FROM net WHERE username=%s ",username)
 
 
 
+
+
+class DefaultHandler(BaseHandler):
+    def get(self):
+        output={'status':'404 not found'}
+        self.write(output)
+
+    def post(self):
+        output={'status':'404 not found'}
+        self.write(output)
 
 class connect(BaseHandler):
     def post(self):
@@ -65,8 +73,40 @@ class connect(BaseHandler):
 
 class login(BaseHandler):
     def post(self):
-        output={'status':'oooooooooohhhhhhh'}
+        output={'status':'oooooooooohhhhhhh','id':'sajjad'}
         self.write(output)
+
+class logout(BaseHandler):
+    # db=Database.DATABASE_
+    def post(self,*args,**kwargs):
+        username=self.get_argument('username')
+        print("hellow ",username)
+        password=self.get_argument('password')
+        print('your passowod is',password)
+
+        output={username:password}
+        self.write(output)
+
+        # else:
+            # logout.db.execute("DELETE from potluck  where name='%s';",args[0])
+
+
+class sendticket(BaseHandler):
+    database=Database.DATABASE_
+    def get(self,*args,**kwargs):
+        token=self.get_argument('token')
+        subject=self.get_argument('subject')
+        body=self.get_argument('body')
+
+        if  not self.check_api(token):
+            output={"staus":"this token not found"}
+            self.write(output)
+
+        else:
+            sendticket.database.execute("INSERT IN TO ticket (token,subject,body)" "values (%s,%s,%s)",token,subject,body)
+
+
+
 
 def main():
     tornado.options.parse_command_line()
